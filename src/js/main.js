9102b155d272a36986168936addde8874479925e
DefineDepth <masterov.mixail@gmail.com>
2021-07-21 05:02:53 +0300

    last

--- src/js/main.js
+++ src/js/main.js
@@ -7,24 +7,16 @@ import barba from '@barba/core';
 import { gsap } from "gsap";
 import Swiper from 'swiper/bundle';
 import LocomotiveScroll from 'locomotive-scroll';
-
-import * as THREE from "three";
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader.js';
-import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
-// import Scene from "./scene";
-
-// import Scene from "./scene";
-// const scene = new Scene("container");
+// import {Curtains, Plane} from 'curtainsjs';
+import {Curtains, Plane, Vec2, Vec3, PingPongPlane, ShaderPass, FXAAPass} from 'curtainsjs';
+import * as PIXI from 'pixi.js'
 
 
 window.APP = {};
 
 APP.html = document.querySelector('html');
 APP.body = document.querySelector('body');
-APP.scroll = scroll(document.querySelector('[data-scroll-container]'));
+// APP.scroll = scroll(document.querySelector('[data-scroll-container]'));
 
 function initComponents() {
   Feather.replace();
@@ -32,13 +24,6 @@ function initComponents() {
   headerBar();
   clientsSectionSlider();
   servicesSectionSlider();
-
-
-
-  // const scene = new Scene("container");
-  
-
-
 }
 
 initComponents();
@@ -280,12 +265,62 @@ function scroll(container) {
 
 
 
-// distortion();
 
-// function distortion() {
-//   const target = document.querySelector('.js-distortion');
+
+
+
+
+
+// var app = new PIXI.Application(window.innerWidth, window.innerHeight);
+// document.body.appendChild(app.view);
+// app.stage.interactive = true;
+// var posX, displacementSprite, displacementFilter, bg, vx;
+// var container = new PIXI.Container();
+// app.stage.addChild(container);
+// app.loader.add("../img/ripple.png").add("img/ginger.jpg").load(setup);
+
+// function setup() {
+//   posX = app.renderer.width / 2;
+//   displacementSprite = new PIXI.Sprite.from('../img/ripple.png');
+//   displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+//   displacementSprite.anchor.set(0.5);
+//   displacementSprite.x = app.renderer.width / 2;
+//   displacementSprite.y = app.renderer.height / 2;
+
+//   vx = displacementSprite.x;
+//   app.stage.addChild(displacementSprite);
+//   container.filters = [displacementFilter];
+//   displacementFilter.scale.x = 0;
+//   displacementFilter.scale.y = 0;
+
+//   bg = new PIXI.Sprite.from('img/ginger.jpg');
+//   bg.width = app.renderer.width;
+//   bg.height = app.renderer.height;
+//   container.addChild(bg);
+
+//   app.stage.on('mousemove', onPointerMove).on('touchmove', onPointerMove);
+//   loop();
 // }
 
+// function onPointerMove(eventData) {
+//   posX = eventData.data.global.x;
+// }
+
+// function loop() {
+//   requestAnimationFrame(loop);
+//   vx += (posX - displacementSprite.x) * 0.045;
+//   displacementSprite.x = vx;
+//   var disp = Math.floor(posX - displacementSprite.x);
+//   if (disp < 0) disp = -disp;
+//   var fs = map(disp, 0, 500, 0, 120);
+//   disp = map(disp, 0, 500, 0.1, 0.6);
+//   displacementSprite.scale.x = disp;
+//   displacementFilter.scale.x = fs;
+// }
+// let map = function(n, start1, stop1, start2, stop2) {
+//   var newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+//   return newval;
+// };
 
 
 
@@ -293,216 +328,222 @@ function scroll(container) {
 
 
 
-const fragment = `
-uniform vec2 u_resolution;
-
-uniform sampler2D u_texture;
-uniform sampler2D u_texture2;
-uniform vec2 u_textureFactor;
-uniform vec2 u_texture2Factor;
-uniform float u_textureProgress;
-
-// RGB
-uniform vec2 u_rgbPosition;
-uniform vec2 u_rgbVelocity;
-
-varying vec2 vUv;
-vec2 centeredAspectRatio(vec2 uvs, vec2 factor){
-    return uvs * factor - factor /2. + 0.5;
-}
-void main(){
-    // On THREE 102 The image is has Y backwards
-    // vec2 flipedUV = vec2(vUv.x,1.-vUv.y);
-
-    vec2 normalizedRgbPos = u_rgbPosition / u_resolution;
-    normalizedRgbPos.y = 1. - normalizedRgbPos.y; 
-
-    
-    vec2 vel = u_rgbVelocity;
-    float dist = distance(normalizedRgbPos + vel / u_resolution, vUv.xy);
-
-    float ratio = clamp(1.0 - dist * 5., 0., 1.);
-
-
-    vec4 tex1 = vec4(1.);
-    vec4 tex2 = vec4(1.);
-
-    vec2 uv = vUv;
-
-    uv.x -= sin(uv.y) * ratio / 100. * (vel.x + vel.y) / 7.;
-    uv.y -= sin(uv.x) * ratio / 100. * (vel.x + vel.y) / 7.;
-
-    tex1.r = texture2D(u_texture, centeredAspectRatio(uv, u_textureFactor )).r;
-    tex2.r = texture2D(u_texture2, centeredAspectRatio(uv, u_textureFactor )).r;
-
-    
-    uv.x -= sin(uv.y) * ratio / 150. * (vel.x + vel.y) / 7.;
-    uv.y -= sin(uv.x) * ratio / 150. * (vel.x + vel.y) / 7.;
-
-    tex1.g = texture2D(u_texture, centeredAspectRatio(uv, u_textureFactor )).g;
-    tex2.g = texture2D(u_texture2, centeredAspectRatio(uv, u_textureFactor )).g;
-    
-    uv.x -= sin(uv.y) * ratio / 300. * (vel.x + vel.y) / 7.;
-    uv.y -= sin(uv.x) * ratio / 300. * (vel.x + vel.y) / 7.;
-
-    tex1.b = texture2D(u_texture, centeredAspectRatio(uv, u_textureFactor )).b;
-    tex2.b = texture2D(u_texture2, centeredAspectRatio(uv, u_textureFactor )).b;
-     
-    
-
-
-    vec4 fulltex1 = texture2D(u_texture, centeredAspectRatio(vUv, u_textureFactor) );
-    vec4 fulltex2 = texture2D(u_texture2, centeredAspectRatio(vUv, u_texture2Factor));
-    
-    vec4 mixedTextures =  mix(tex1,tex2,u_textureProgress);
-
-    gl_FragColor = mixedTextures;
-}
-`;
-
-const vertex = `
-#define PI 3.14159265359
-uniform float u_offset;
-uniform float u_progress;
-uniform float u_direction;
-uniform float u_time;
-uniform float u_waveIntensity;
-varying vec2 vUv;
-void main(){
-    vec3 pos = position.xyz;
-
-    float distance = length(uv.xy - 0.5 );
-    float sizeDist = length(vec2(0.5,0.5));
-    float normalizedDistance = distance/sizeDist ;
-
-    float stickOutEffect = normalizedDistance ;
-    float stickInEffect = -normalizedDistance ;
-
-    
-    float stickEffect = mix(stickOutEffect,stickInEffect, u_direction);
-
-    // Backwards V wave.
-    float stick = 0.5;
-
-    float waveIn = u_progress*(1. / stick); 
-    float waveOut =  -( u_progress - 1.) * (1./(1.-stick) );
-    waveOut = pow(smoothstep(0.,1.,waveOut),0.7);
-
-    float stickProgress = min(waveIn, waveOut);
 
 
 
 
 
-    // We can re-use stick Influcse because this oen starts at the same position
-    float offsetInProgress = clamp(waveIn,0.,1.);
-
-    // Invert stickout to get the slope moving upwards to the right
-    // and move it left by 1
-    float offsetOutProgress = clamp(1.-waveOut,0.,1.);
-
-    float offsetProgress = mix(offsetInProgress,offsetOutProgress,u_direction);
-
-
-    float stickOffset = u_offset;
-    pos.z += stickEffect * stickOffset * stickProgress  - u_offset * offsetProgress;
-
-    
-    pos.z += sin(distance * 8. - u_time * 2. )  * u_waveIntensity;
-
-    gl_Position =   
-        projectionMatrix * 
-        modelViewMatrix * 
-         vec4(pos, 1.0);
-
-    vUv = uv;
-}
-`;
-
-
-
-
-// const postvertex = document.querySelector('#post-vertex');
-// const postfragment = document.querySelector('#post-fragment');
-
-// const vertex = document.querySelector('#shader-vertex');
-// const fragment = document.querySelector('#shader-fragment');
 
 
 
 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 
+window.addEventListener("load", function() {
 
-const TEXTURE = new THREE.TextureLoader().load('../img/ginger.jpg'); 
-const geometry = new THREE.PlaneBufferGeometry();
+  function lerp (start, end, amt){
+    return (1 - amt) * start + amt * end;
+  }
 
+  // set up our WebGL context and append the canvas to our wrapper
+  var webGLCurtain = new Curtains({
+    container: "canvas"
+  });
 
-// const material = new THREE.MeshBasicMaterial( { map: TEXTURE } );
+  webGLCurtain.onRender(function() {
+    // update our planes deformation
+    // increase/decrease the effect
+    planesDeformations = lerp(planesDeformations, 0, 0.075);
+    scrollEffect = lerp(scrollEffect, 0, 0.075);
 
-const material = new THREE.ShaderMaterial({
-  uniforms: {
-    u_texture: { type: "t", value: TEXTURE },
-    // u_textureFactor: { type: "f", value: this.factors[this.currentIndex] },
-    // u_texture2: { type: "t", value: this.textures[this.nextIndex] },
-    // u_texture2Factor: { type: "f", value: this.factors[this.nextIndex] },
-    u_textureProgress: { type: "f", value: 0 },
-    u_offset: { type: "f", value: 8 },
-    u_progress: { type: "f", value: 0 },
-    u_direction: { type: "f", value: 1 },
-    u_effect: { type: "f", value: 0 },
-    u_time: { type: "f", value: 0 },
-    u_waveIntensity: { type: "f", value: 0 },
-    u_resolution: {
-      type: "v2",
-      value: new THREE.Vector2(window.innerWidth, window.innerHeight)
+  }).onScroll(function() {
+    // get scroll deltas to apply the effect on scroll
+    var delta = webGLCurtain.getScrollDeltas();
+
+    // invert value for the effect
+    delta.y = -delta.y;
+
+    // threshold
+    if(delta.y > 60) {
+      delta.y = 60;
+    }
+    else if(delta.y < -60) {
+      delta.y = -60;
+    }
+
+    if(Math.abs(delta.y) > Math.abs(planesDeformations)) {
+      planesDeformations = lerp(planesDeformations, delta.y, 0.5);
+    }
+  
+    if(Math.abs(delta.y) > Math.abs(scrollEffect)) {
+      scrollEffect = lerp(scrollEffect, delta.y, 0.5);
+    }
+  }).onError(function() {
+    // we will add a class to the document body to display original images
+    document.body.classList.add("no-curtains", "planes-loaded");
+  }).onContextLost(function() {
+    // on context lost, try to restore the context
+    webGLCurtain.restoreContext();
+  });
+
+  // we will keep track of all our planes in an array
+  var planes = [];
+  var planesDeformations = 0
+  var scrollEffect = 0;
+
+  // get our planes elements
+  var planeElements = document.getElementsByClassName("plane");
+
+  var vs = `
+    precision mediump float;
+
+    // default mandatory variables
+    attribute vec3 aVertexPosition;
+    attribute vec2 aTextureCoord;
+
+    uniform mat4 uMVMatrix;
+    uniform mat4 uPMatrix;
+
+    uniform mat4 planeTextureMatrix;
+
+    // custom variables
+    varying vec3 vVertexPosition;
+    varying vec2 vTextureCoord;
+
+    uniform float uPlaneDeformation;
+
+    void main() {
+      vec3 vertexPosition = aVertexPosition;
+
+      // cool effect on scroll
+      vertexPosition.y += sin(((vertexPosition.x + 1.0) / 2.0) * 3.141592) * (sin(uPlaneDeformation / 250.0));
+
+      gl_Position = uPMatrix * uMVMatrix * vec4(vertexPosition, 1.0);
+
+      // varyings
+      vVertexPosition = vertexPosition;
+      vTextureCoord = (planeTextureMatrix * vec4(aTextureCoord, 0.0, 1.0)).xy;
+    }
+  `;
+
+  var fs = `
+    precision mediump float;
+
+    varying vec3 vVertexPosition;
+    varying vec2 vTextureCoord;
+
+    uniform sampler2D planeTexture;
+
+    void main() {
+      // just display our texture
+      gl_FragColor = texture2D(planeTexture, vTextureCoord);
+    }
+  `;
+
+  // all planes will have the same parameters
+  var params = {
+    vertexShader: vs,
+    fragmentShader: fs,
+    shareProgram: true, // share planes program to improve plane creation speed
+    widthSegments: 10,
+    heightSegments: 10,
+    drawCheckMargins: {
+      top: 100,
+      right: 0,
+      bottom: 100,
+      left: 0,
     },
-    u_rgbPosition: {
-      type: "v2",
-      value: new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2)
+    uniforms: {
+      planeDeformation: {
+        name: "uPlaneDeformation",
+        type: "1f",
+        value: 0,
+      },
+    }
+  };
+
+  // add our planes and handle them
+  for (var i = 0; i < planeElements.length; i++) {
+    planes.push(webGLCurtain.addPlane(planeElements[i], params));
+
+    handlePlanes(i);
+  }
+
+  // handle all the planes
+  function handlePlanes(index) {
+    var plane = planes[index];
+
+    // check if our plane is defined and use it
+    plane && plane.onLoading(function() {
+      //console.log(plane.loadingManager.sourcesLoaded);
+    }).onReady(function() {
+      plane.setRenderTarget(rgbTarget);
+
+      // once everything is ready, display everything
+      if(index === planes.length - 1) {
+        document.body.classList.add("planes-loaded");
+      }
+    }).onRender(function() {
+      // update the uniform
+      plane.uniforms.planeDeformation.value = planesDeformations;
+    
+      //plane.setScale(1, 1 + Math.abs(scrollEffect) / 500);
+      plane.textures[0].setScale(1 + Math.abs(scrollEffect) / 500);
+    });
+  }
+
+  var rgbFs = `
+    precision mediump float;
+
+    varying vec3 vVertexPosition;
+    varying vec2 vTextureCoord;
+
+    uniform sampler2D uRenderTexture;
+
+    uniform float uScrollEffect;
+
+    void main() {
+      vec2 textureCoords = vTextureCoord;
+
+      vec2 redTextCoords = vec2(vTextureCoord.x, vTextureCoord.y - uScrollEffect / 500.0);
+      vec2 greenTextCoords = vec2(vTextureCoord.x, vTextureCoord.y - uScrollEffect / 1000.0);
+      vec2 blueTextCoords = vec2(vTextureCoord.x, vTextureCoord.y - uScrollEffect / 1500.0);
+
+      vec4 red = texture2D(uRenderTexture, redTextCoords);
+      vec4 green = texture2D(uRenderTexture, greenTextCoords);
+      vec4 blue = texture2D(uRenderTexture, blueTextCoords);
+
+      vec4 finalColor = vec4(red.r, green.g, blue.b, min(1.0, red.a + blue.a + green.a));
+      gl_FragColor = finalColor;
+    }
+  `;
+
+  var rgbTarget = webGLCurtain.addRenderTarget();
+
+
+  var rgbPass = webGLCurtain.addShaderPass({
+    fragmentShader: rgbFs,
+    renderTarget: rgbTarget,
+    depthTest: false, // we need to disable the depth test to display that shader pass on top of the first one
+    uniforms: {
+      scrollEffect: {
+        name: "uScrollEffect",
+        type: "1f",
+        value: 0,
+      },
     },
-    u_rgbVelocity: { type: "v2", value: new THREE.Vector2(0, 0) }
-  },
-  vertexShader: vertex,
-  fragmentShader: fragment,
-  side: THREE.DoubleSide,
-  // map: TEXTURE,
+  });
+
+  if ( rgbPass ) {
+    rgbPass.onRender(function() {
+      // update the uniform
+      rgbPass.uniforms.scrollEffect.value = scrollEffect;
+    });
+  }
+
 });
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-// set up post processing
-let composer = new EffectComposer(renderer);
-let renderPass = new RenderPass(scene, camera);
-// rendering our scene with an image
-composer.addPass(renderPass);
-
-// our custom shader pass for the whole screen, to displace previous render
-// let customPass = new ShaderPass({vertexShader,fragmentShader});
-let customPass = new ShaderPass( fragment );
-customPass.renderToScreen = true;
-composer.addPass( customPass );
-
-
-camera.position.z = 1;
-
-function animate() {
-  requestAnimationFrame( animate );
-
-  // mesh.rotation.x += 0.01;
-  // mesh.rotation.y += 0.01;
-
-  composer.render();
-}
-animate();
 
 
 
@@ -514,28 +555,108 @@ animate();
 
 
 
-// getTwitchFollowers();
-
-// function getTwitchFollowers() {
-
-
-//   console.log('oppa');
-// }
 
 
 
-// // App.SMcontroller = new ScrollMagic.Controller();
 
 
-// window.onload = function () {
-//   // customEasingsInit();
 
-//   document.fonts.ready.then(function () {
-//     initComponents();
-//     // initialReveal();
+
+
+
+
+
+
+
+
+
+
+
+// const vs = `
+//   precision mediump float;
+//   // those are the mandatory attributes that the lib sets
+//   attribute vec3 aVertexPosition;
+//   attribute vec2 aTextureCoord;
+//   // those are mandatory uniforms that the lib sets and that contain our model view and projection matrix
+//   uniform mat4 uMVMatrix;
+//   uniform mat4 uPMatrix;
+//   // our texture matrix that will handle image cover
+//   uniform mat4 uTextureMatrix0;
+//   // pass your vertex and texture coords to the fragment shader
+//   varying vec3 vVertexPosition;
+//   varying vec2 vTextureCoord;
+//   void main() {
+//     vec3 vertexPosition = aVertexPosition;
+//     gl_Position = uPMatrix * uMVMatrix * vec4(vertexPosition, 1.0);
+//     // set the varyings
+//     // here we use our texture matrix to calculate the accurate texture coords
+//     vTextureCoord = (uTextureMatrix0 * vec4(aTextureCoord, 0.0, 1.0)).xy;
+//     vVertexPosition = vertexPosition;
+//   }
+// `;
+
+// const fs = `
+//   precision mediump float;
+//   // get our varyings
+//   varying vec3 vVertexPosition;
+//   varying vec2 vTextureCoord;
+//   // the uniform we declared inside our javascript
+//   uniform float uTime;
+//   // our texture sampler (default name, to use a different name please refer to the documentation)
+//   uniform sampler2D uSampler0;
+//   void main() {
+//     // get our texture coords from our varying
+//     vec2 textureCoord = vTextureCoord;
+//     // displace our pixels along the X axis based on our time uniform
+//     // textures coords are ranging from 0.0 to 1.0 on both axis
+//     textureCoord.x += sin(textureCoord.y * 25.0) * cos(textureCoord.x * 25.0) * (cos(uTime / 50.0)) / 25.0;
+//     // map our texture with the texture matrix coords
+//     gl_FragColor = texture2D(uSampler0, textureCoord);
+//   }
+// `;
+
+
+
+
+
+
+
+
+
+
+// wait for everything to be ready
+// window.addEventListener("load", () => {
+//   // set up our WebGL context and append the canvas to our wrapper
+//   const curtains = new Curtains({
+//     container: "canvas",
+//     pixelRatio: Math.min(1.5, window.devicePixelRatio), // limit pixel ratio for performance
+//     autoRender: false, // use gsap ticker to render our scene
 //   });
-// }
 
-// function initComponents() {
-//   console.log('Init components');
-// }
+//   gsap.ticker.add(curtains.render.bind(curtains));
+
+//   // get our plane element
+//   const planeElement = document.getElementsByClassName("plane")[0];
+
+//   // set our initial parameters (basic uniforms)
+//   const params = {
+//     vertexShader: vs,
+//     fragmentShader: fs,
+//     uniforms: {
+//       time: {
+//         name: "uTime", // uniform name that will be passed to our shaders
+//         type: "1f", // this means our uniform is a float
+//         value: 0,
+//       },
+//     },
+//   };
+
+//   // create our plane using our curtains object, the HTML element and the parameters
+//   const plane = new Plane(curtains, planeElement, params);
+
+//   plane.onRender(() => {
+//     // use the onRender method of our plane fired at each requestAnimationFrame call
+//     plane.uniforms.time.value++; // update our time uniform value
+//   });
+// });
+
